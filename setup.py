bb4e9094febe8521c3f8cab07f3e2f2c4ae11019
Ahmed Kamel <k.tricky@gmail.com>
2015-03-27 17:35:21 +0200

    added salt as a dependency

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='newrelic_ops',
-      version='0.3.dev',
+      version='0.4.dev',
       description='newrelic manipulation tool',
       long_description=open('README.md').read(),
       keywords='newrelic install saltstack automatic',
@@ -10,7 +10,7 @@ setup(name='newrelic_ops',
       author_email='k.tricky@gmail.com',
       license='MIT',
       packages=['newrelic_ops'],
-      install_requires=['cement'],
+      install_requires=['cement', 'salt'],
       scripts=['bin/new_relic'],
       include_package_data=True,
       zip_safe=False)
